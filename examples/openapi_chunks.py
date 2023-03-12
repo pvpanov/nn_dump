554c704ee3c43b83f15921797e4665cfb9d986ef
Petr Panov <pvpanov93@gmail.com>
2023-03-12 22:04:15 +0100

    add a simple dash

--- examples/openapi_chunks.py
+++ examples/openapi_chunks.py
@@ -1,9 +1,13 @@
 import requests
+import argparse
+
 from personal_keys import openai_api_key
+
 
 # REF: https://platform.openai.com/docs/api-reference/completions
 
-def calculator_promt():
+
+def calculator_prompt(prompt):
     response = requests.post(
         url="https://api.openai.com/v1/completions",  # api endpoint
         headers={
@@ -12,13 +16,17 @@ def calculator_promt():
         },
         json={
             "model": "text-davinci-003",  # ML model to use
-            "prompt": "Write a calculator in Python",
-            "max_tokens": 100,  #
-            "temperature": 0.5,  # "creativeness"
+            "prompt": f"Write python script to {prompt}",
+            "max_tokens": 1000,  #
+            "temperature": 0.2,  # "creativeness"
         },
     )
     assert response.status_code == 200
     return response.json()["choices"][0]["text"]
 
+
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="The prompt to send to the OpenAI API")
+    args = parser.parse_args()
+    print(calculator_prompt(args.prompt))
